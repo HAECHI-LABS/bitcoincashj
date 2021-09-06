999b3152e291a6191794ce893b97af7d05e3f4af
AgwaB <leesd556@gmail.com>
2021-09-06 23:04:34 +0900

    henesis base branch

--- wallettemplate/src/main/java/wallettemplate/utils/BitcoinUIModel.java
+++ wallettemplate/src/main/java/wallettemplate/utils/BitcoinUIModel.java
@@ -21,12 +21,12 @@ import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import org.bitcoinj.core.Address;
-import org.bitcoinj.core.Coin;
-import org.bitcoinj.core.listeners.DownloadProgressTracker;
-import org.bitcoinj.wallet.Wallet;
-import org.bitcoinj.wallet.listeners.CurrentKeyChangeEventListener;
-import org.bitcoinj.wallet.listeners.WalletChangeEventListener;
+import org.bitcoincashj.core.Address;
+import org.bitcoincashj.core.Coin;
+import org.bitcoincashj.core.listeners.DownloadProgressTracker;
+import org.bitcoincashj.wallet.Wallet;
+import org.bitcoincashj.wallet.listeners.CurrentKeyChangeEventListener;
+import org.bitcoincashj.wallet.listeners.WalletChangeEventListener;
 
 import java.util.Date;
 
